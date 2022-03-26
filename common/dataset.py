a54d09b7c046d3fa4697f1ba2f54a129d4391a8e
goodboyyes2009 <704636682@qq.com>
2022-03-26 13:46:13 +0800

    refactor(): 重构处理新闻数据的ChineseNewsData, 并添加相应的测试

---
+++ common/dataset.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+import torch.utils
+import os
+import numpy as np
+from common.tokenization import get_stop_words, Vocabulary, token_function
+
+
+class ChineseNewsData(torch.utils.data.Dataset):
+    """Chinese News dataset."""
+
+    train_file_name = "train.tsv"
+    test_file_name = "test.tsv"
+
+    def __init__(self, has_header=False, split_char=",", data_root_path=None, train=True, transforms=None):
+        super().__init__()
+        self.hasHeader = has_header
+        self.data_root_path = data_root_path
+        self.train = train
+        self.transforms = transforms
+        self.split_char = split_char
+
+        self.data = []
+        self.targets = []
+
+        file_name = self.train_file_name if train else self.test_file_name
+
+        with open(os.path.join(self.data_root_path, file_name), 'r', encoding='utf-8') as f:
+            for line in f:
+                try:
+                    if self.hasHeader:
+                        continue
+                    splits = line.split(self.split_char)
+                    if len(splits) < 2:
+                        continue
+                    if len(splits[0].strip()) == 0:
+                        continue
+                    self.data.append(splits[0].strip())
+                    target = splits[1].strip().replace('\n', '')
+                    self.targets.append(int(target))
+                except Exception as e:
+                    print("解析{}数据发生异常，异常信息为:{}".format(file_name, e))
+
+    def __len__(self):
+        """ 返回整个数据的长度 """
+        return len(self.data)
+
+    def __getitem__(self, index):
+        """
+        :param index: 数据在list中的下标
+        :return: a tuple, (text, label)
+        """
+        text, label = self.data[index], self.targets[index]
+        if self.transforms is not None:
+            text = self.transforms(text)
+        if self.transforms is not None:
+            label = self.transforms(label)
+        return text, label
+
+
+# 定义一些transforms
+# tokenize
+class Tokenize(object):
+    """
+    对文本句子进行分词，然后进行onehot编码
+    """
+    stop_words = get_stop_words()
+    vocabulary = Vocabulary(stop_words=stop_words, token_fn=token_function)
+
+    def __call__(self, sample):
+        # label 为int类型, 因此下面的条件不成立
+        if isinstance(sample, str):
+            # 只对text进行onehot编码变成input_id
+            sample = self.vocabulary.encode(sample)
+        return sample
+
+
+class ToTensor(object):
+    """
+    将input_id(List[int])或者label(int)转换成tensor
+    """
+
+    def __init__(self, device):
+        print("ToTensor device:{}".format(device))
+        self._device = device
+
+    def __call__(self, sample):
+        if isinstance(sample, np.ndarray):
+            # 将input_id转换成LongTensor
+            sample = torch.from_numpy(sample).long()
+            sample = torch.tensor(sample, device=self._device)
+        elif isinstance(sample, int):
+            # 将label转换成LongTensor
+            sample = torch.tensor(sample, device=self._device).long()
+        return sample
---
+++ test/common/dataset_test.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import torch
+from torch.utils.data import DataLoader
+from common.dataset import ChineseNewsData
+
+
+def test_chinese_news_dataset():
+    data_root_path = '/home/hj/dataset/news_data/news_zh'
+
+    news_data = ChineseNewsData(split_char='\t', data_root_path=data_root_path,
+                                transforms=None)
+    for i in range(len(news_data)):
+        sample = news_data[i]
+        text, label = sample[0], sample[1]
+        # 打印前4条
+        if i > 3:
+            break
+        print("text:{}, label:{}".format(text, label))
+
+
+def test_torchvision_transforms():
+    from torchvision import transforms, utils
+    from common.dataset import ToTensor, Tokenize
+    data_root_path = '/home/hj/dataset/news_data/news_zh'
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device("cpu")
+
+    news_data = ChineseNewsData(split_char='\t', data_root_path=data_root_path,
+                                transforms=transforms.Compose([Tokenize(), ToTensor(device=device)]))
+
+    print(len(news_data))
+
+    for i in range(len(news_data)):
+        sample = news_data[i]
+        print("iter sample: {}".format(sample))
+        text, label = sample[0], sample[1]
+        # 打印前2条
+        if i > 1:
+            break
+        print("text:{}, label:{}".format(text, label))
+
+
+def test_dataloader_batch_sampler():
+    from common.dataset import Tokenize, ToTensor
+    # 需要添加下面这一行代码，否则报下面的错误
+    # RuntimeError: Cannot re-initialize CUDA in forked subprocess. To use CUDA with multiprocessing, you must use the 'spawn' start method
+    torch.multiprocessing.set_start_method('spawn')
+    from torchvision import transforms, utils
+    data_root_path = '/home/hj/dataset/news_data/news_zh'
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device("cpu")
+
+    news_data = ChineseNewsData(split_char='\t', data_root_path=data_root_path,
+                                transforms=transforms.Compose([Tokenize(), ToTensor(device=device)]))
+
+    # Batch sample
+    dataloader = DataLoader(news_data, batch_size=4,
+                            shuffle=True, num_workers=0)
+
+    for index_batch, sample_batched in enumerate(dataloader):
+        print("index: {}, len of sample_batched: {}".format(index_batch, len(sample_batched)))
