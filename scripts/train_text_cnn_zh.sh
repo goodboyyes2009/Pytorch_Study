28acc1bf6eae02df51e44d81ef859ae42293517e
goodboyyes2009 <704636682@qq.com>
2022-03-29 22:19:36 +0800

    fix(): 修复脚本

--- scripts/train_text_cnn_zh.sh
+++ scripts/train_text_cnn_zh.sh
@@ -4,4 +4,4 @@ python  ../cnn/train_text_cnn_zh.py \
         --filter_sizes 3 4 5 \
         --max_epochs 100 \
         --word_embedding tencent \
-        --data_root_path=/home/hj/data/nlp/news_data
+        --data_root_path=/home/hj/dataset/news_data/news_zh
