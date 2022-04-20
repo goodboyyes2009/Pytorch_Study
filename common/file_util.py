68b2447a1a8f381c1eac50f73fbcba50b8e64dce
goodboyyes2009 <704636682@qq.com>
2022-04-20 17:56:25 +0800

    refactor(): 重构pickle#load, pickle#save方法

--- common/file_util.py
+++ common/file_util.py
@@ -12,19 +12,28 @@ logger = logging.getLogger(__name__)
 def save_pickle(obj, save_path):
     if os.path.exists(save_path):
         logger.warning("文件{}已存在!".format(save_path))
+        raise FileExistsError("文件{}已存在!".format(save_path))
     else:
-        save_file = open(save_path, 'wb', encoding='utf-8')
-        pickle.dump(save_file, save_path)
-        save_file.close()
-        logger.info("文件{}已保存成功!".format(save_path))
+        try:
+            save_file = open(save_path, 'wb')
+            pickle.dump(obj, save_file)
+            save_file.close()
+            logger.info("文件{}已保存成功!".format(save_path))
+        except Exception as e:
+            logger.error("保存文件:{}失败, 异常信息:{}".format(save_path, e))
 
 
 def load_pickle(load_path):
     if not os.path.exists(load_path):
         logger.error("load path: {}不存在!".format(load_path))
+        raise FileNotFoundError("load path: {}不存在!".format(load_path))
     else:
-        load_file = open(load_path, 'rb', encoding='utf-8')
-        pickle_obj = pickle.load(load_path)
-        load_file.close()
-        logger.info("加载{}文件成功!".format(load_path))
+        pickle_obj = None
+        try:
+            load_file = open(load_path, 'rb')
+            pickle_obj = pickle.load(load_file)
+            load_file.close()
+            logger.info("加载{}文件成功!".format(load_path))
+        except Exception as e:
+            logger.error("加载{}文件失败,异常信息:{}".format(load_path, e))
         return pickle_obj
