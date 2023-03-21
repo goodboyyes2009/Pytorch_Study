2cfdbfa5bd34fab1871ab11d70e119c3cce43d6a
goodboyyes2009 <704636682@qq.com>
2023-03-21 10:39:28 +0800

    feat(): torch word language model example

--- language_model/word_language_model/README.md
+++ language_model/word_language_model/README.md
@@ -9,3 +9,8 @@ python main.py --cuda --epochs 6
 ```shell
 python main.py --cuda --epochs 2 --model Transformer --lr 5
 ```
+
+## 使用训练好的模型生成文本
+```shell
+python generate.py
+```
--- language_model/word_language_model/generate.py
+++ language_model/word_language_model/generate.py
@@ -33,3 +33,38 @@ with open(args.checkpoint, 'rb') as f:
 # 开启评估模式
 model.eval()
 
+# 词汇表
+vocab_dict = data_helper.Corpus(args.data).dictionary
+num_of_tokens = len(vocab_dict)
+
+is_transformer_model = hasattr(model, 'model_type') and model.model_type == 'Transformer'
+print("is_transformer_model: {}".format(is_transformer_model))
+
+if not is_transformer_model:
+    hidden = model.init_hidden(1)
+
+# 从词表中随机选取一个字作为生成文本的输入字符
+input_text = torch.randint(num_of_tokens, (1, 1), dtype=torch.long).to(device)
+
+with open(args.outf, 'w', encoding='utf-8') as f:
+    with torch.no_grad():
+        for i in range(args.words):
+            if is_transformer_model:
+                output = model(input_text, has_mask=False)
+                word_weights = output[-1].squeeze().div(args.temperature).exp().cpu()
+                # torch.multinominal方法可以根据给定权重对数组进行多次采样，返回采样后的元素下标
+                word_idx = torch.multinomial(input=word_weights, num_samples=1)[0]
+                word_tensor = torch.Tensor([[word_idx]]).long().to(device)
+                input_text = torch.cat(tensors=[input_text, word_tensor], dim=0)
+            else:
+                output, hidden = model(input_text, hidden)
+                word_weights = output.squeeze().div(args.temperature).exp().cpu()
+                word_idx = torch.multinomial(input=word_weights, num_samples=1)[0]
+                input_text.fill_(word_idx)
+
+            word = vocab_dict.id2word[word_idx]
+
+            f.write(word + ('\n' if i % 20 == 19 else ' '))
+
+            if i % args.log_interval == 0:
+                print("| Generate {}/{} words".format(i, args.words))
